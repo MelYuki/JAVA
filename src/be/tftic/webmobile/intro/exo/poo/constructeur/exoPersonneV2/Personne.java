80384976b8e76b047c73e7dbd45fdcfd4ace45e9
melvin taider <tmneo1989@hotmail.com>
2023-06-29 10:07:44 +0200

    Update_ExoPersonneV2

--- src/be/tftic/webmobile/intro/exo/poo/constructeur/exoPersonneV2/Personne.java
+++ src/be/tftic/webmobile/intro/exo/poo/constructeur/exoPersonneV2/Personne.java
@@ -2,10 +2,13 @@ package be.tftic.webmobile.intro.exo.poo.constructeur.exoPersonneV2;
 
 public class Personne {
 
+    // region ATTRIBUS
     String nom;
     String prenom;
     int age = 35;
+    // endregion
 
+    // region CONSTRUCTEUR
     Personne(){}
     Personne(String nom, String prenom){
         this.nom = nom;
@@ -16,13 +19,18 @@ public class Personne {
         this.prenom = prenom;
         this.age = age;
     }
+    // endregion
 
-
-    void saluer(){
+    // region METHODS
+    /*void saluer(){
 
         System.out.println("  Bonjour!");
     }
+    boolean bjr = true;
     void saluer(boolean sePresenter){
+
+        if(bjr)
+            saluer();
 
         if(sePresenter)
             System.out.printf("  Je m'appelle %s %s et j'ai %d ans.\n",
@@ -30,8 +38,6 @@ public class Personne {
                     this.nom,
                     this.age
             );
-        else
-            saluer();
     }
     void saluer (Personne aSaluer) {
 
@@ -45,7 +51,38 @@ public class Personne {
         saluer(aSaluer);
 
         if(sePresenter) {
-            saluer(sePresenter);
+            bjr = false;
+            saluer(true);
         }
+    }*/
+    // endregion
+
+    // region ALEX METHODS
+    void saluer(){
+        this.saluer(null, false);
     }
+
+    void saluer(Personne aSaluer){
+        this.saluer(aSaluer, false);
+    }
+
+    void saluer(boolean sePresenter){
+        this.saluer(null, sePresenter);
+    }
+
+    void saluer(Personne aSaluer, boolean sePresenter){
+
+        String message = sePresenter ?
+                ",je suis %s %s et j'ai %d ans.".formatted(this.prenom, this.nom, this.age)
+                : "!";
+
+        if( aSaluer != null )
+            message = "Bonjour %s %s".formatted(aSaluer.prenom, aSaluer.nom) + message;
+        else
+            message = "Bonjour" + message;
+
+        System.out.println(message);
+
+    }
+    // endregion
 }
