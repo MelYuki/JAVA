9d98e423aed56616bb8b7f02a7ed38d5dbb4daf0
melvin taider <tmneo1989@hotmail.com>
2023-06-30 16:03:57 +0200

    6th round: heritage

--- src/be/tftic/webmobile/intro/demo/poo/heritage/Employe.java
+++ src/be/tftic/webmobile/intro/demo/poo/heritage/Employe.java
@@ -2,5 +2,25 @@ package be.tftic.webmobile.intro.demo.poo.heritage;
 
 public class Employe extends Personne{
 
+    private int salaire;
+
+    public Employe(String nom, String prenom, int salaire) {
+        super(nom, prenom);
+//        super();
+        this.salaire = salaire;
+    }
+
+    @Override // Permet de produire une erreur de compilation si ce n'est pas une réécriture
+    public String getSalutation(){
+        return "Salut!";
+    }
+
+    public int getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(int salaire) {
+        this.salaire = salaire;
+    }
 
 }
---
+++ src/be/tftic/webmobile/intro/demo/poo/heritage/Manager.java
@@ -0,0 +1,9 @@
+package be.tftic.webmobile.intro.demo.poo.heritage;
+
+public class Manager extends Employe {
+
+    public Manager(){
+        super("Boss", "Big", 255594);
+    }
+
+}
