f4c16c97e65d65ab8af5d6afcec9ffc14f96876a
melvin taider <tmneo1989@hotmail.com>
2023-07-07 11:08:27 +0200

    demoException_end

---
+++ src/be/tftic/webmobile/intro/demo/utils/demoCauseException.java
@@ -0,0 +1,25 @@
+package be.tftic.webmobile.intro.demo.utils;
+
+public class demoCauseException {
+
+    public static void main(String[] args) {
+        methodA();
+    }
+
+    public static void methodA(){
+        try {
+            methodB();
+        }
+        catch (Exception e){
+            throw new RuntimeException("Moins bizarre", e);
+        }
+        finally {
+            System.out.println("Executé dans tous les cas");
+        }
+    }
+
+    public static void methodB(){
+        throw new RuntimeException("Bizarre");
+    }
+
+}
